5143e34a297dfbd269f39680aaac64f3de7f58de
luisalvs <luisalvst@gmail.com>
2025-08-29 16:07:04 -0300

    Create main.py

---
+++ Mini Banco/main.py
@@ -0,0 +1,11 @@
+# O sistema precisa ter:
+
+# Cadastro de contas → cada conta com número, titular e saldo.
+
+# Depósito → adicionar valor a uma conta.
+
+# Saque → retirar valor, se houver saldo suficiente.
+
+# Transferência → passar valor de uma conta para outra.
+
+# Listar contas → ver todas as contas e saldos.
