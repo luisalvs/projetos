edb99c718e41d02a6b56b138fd0d653ae9b5ace7
luisalvs <luisalvst@gmail.com>
2025-09-01 12:03:44 -0300

    Update main.py

--- Mini Banco/main.py
+++ Mini Banco/main.py
@@ -1,13 +1,30 @@
-# O sistema precisa ter:
+from modulo import *
 
-# Cadastro de contas → cada conta com número, titular e saldo.
+print("Bem vindo ao LSV BANCO")
+print("Como podemos te ajudar hoje?")
 
-# Depósito → adicionar valor a uma conta.
-
-# Saque → retirar valor, se houver saldo suficiente.
-
-# Transferência → passar valor de uma conta para outra.
-
-# Listar contas → ver todas as contas e saldos.
-
-
+exit = True
+while exit:
+    print("""
+1 - Cadastrar conta
+2 - Realizar um depósito
+3 - Realizar um saque
+4 - Realizar um transferência
+5 - Listar contas
+6 - Sair           
+""")
+    
+    opcoes = int(input('Escolha uma das opções acima: '))
+    match opcoes:
+        case 1:
+            cadastrar_conta()
+        case 2:
+            depositar()
+        case 3:
+            saque()
+        case 4:
+            transferencia()
+        case 5:
+            listar_contas()
+        case _:
+            exit = False
