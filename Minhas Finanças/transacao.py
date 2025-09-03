c3c2604e3bc48550940f54a8103f6037a0bfd53d
luisalvs <luisalvst@gmail.com>
2025-09-03 13:34:53 -0300

    Create transacao.py

---
+++ Minhas Finanças/transacao.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from categoria import Categoria
+
+@dataclass
+class Transacao:
+    descricao: str
+    valor: float
+    categoria : Categoria
+
+    # def exibir(self):
+    #     print(f'Descrição: {self.descricao} | Valor: {self.valor} | Categoria: {self.categoria.nome}')
